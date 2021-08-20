a27c406b76cdc95d8690c13cd751ab5d2a62c3ae
RitaUJ <82182749+RitaUJ@users.noreply.github.com>
2021-08-20 16:29:54 +0100

    Add project files.

---
+++ PDEMO1/Features/ProteinWorks.feature
@@ -0,0 +1,10 @@
+﻿Feature: ProteinWorks
+	As a new user to the website
+	I want to register an account
+	So that I can login to the website
+
+@mytag
+Scenario: Create an Account
+	Given I fill in my details
+	When I click on JOIN UP
+	Then I should be registered
---
+++ PDEMO1/Steps/ProteinWorksSteps.cs
@@ -0,0 +1,41 @@
+﻿using OpenQA.Selenium;
+using PDEMO1.Hooks;
+using PDEMO1.PageObjects;
+using System;
+using TechTalk.SpecFlow;
+
+namespace PDEMO1.Steps
+{
+    [Binding]
+    public class ProteinWorksSteps
+    {
+        public IWebDriver driver = WebHooks.driver;
+        ProteinWorksPage _proteinWorksPage = new ProteinWorksPage();
+        [Given(@"I fill in my details")]
+        public void GivenIFillInMyDetails()
+        {
+            _proteinWorksPage.EnterFirstName();
+            _proteinWorksPage.EnterLastName();
+            _proteinWorksPage.EnterEmail();
+            _proteinWorksPage.EnterPassword();
+            _proteinWorksPage.EnterCPassword();
+            _proteinWorksPage.SelectGender();
+            _proteinWorksPage.EnterDob();
+            _proteinWorksPage.EnterMobileNumber();
+            _proteinWorksPage.SelectMainGoal();
+            _proteinWorksPage.ClickSubscribeNo();
+        }
+
+        [When(@"I click on JOIN UP")]
+        public void WhenIClickOnJOINUP()
+        {
+            _proteinWorksPage.ClickJoinUp();
+        }
+        
+        [Then(@"I should be registered")]
+        public void ThenIShouldBeRegistered()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
